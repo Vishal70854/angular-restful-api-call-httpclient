8323765f91bf322371ff60c4724127814837cde9
Vishal Kumar Singh <vishalkumar70854@gmail.com>
2026-01-20 19:03:59 +0530

    HttpClient -REST API call | Displaying Data in HTML

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,25 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User implements OnInit {
 
+  users : any;  // list of users object
+
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http.get('https://jsonplaceholder.typicode.com/users')
-      .subscribe({
-        next: (data) => {
-          console.log('Response:', data);
-        },
-        error: (err) => {
-          console.error('Error:', err);
-        }
-      });
+      .subscribe((data)=>this.users=data);
   }
 }
